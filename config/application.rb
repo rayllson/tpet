260a6c99aa20ff4f653e6a6f368be4e4054df4f3
Diego Linhares <diegomlinhares@gmail.com>
2014-09-23 02:26:59 -0300

    alterações parciais

--- config/application.rb
+++ config/application.rb
@@ -19,6 +19,7 @@ module Tpet
     config.i18n.available_locales = [:en, :"pt-BR"]
     config.i18n.default_locale = :"pt-BR"
     config.encoding = "utf-8"
+    
 
     # The default locale is :en and all translations from config/locales/*.rb,yml are auto loaded.
     # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
